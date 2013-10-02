089ef381f623aefdc1277943a6c5c8ef09ebde16
pinalbhatt <self@pbdesk.com>
2013-10-02 13:28:27 -0400

    MVC Project Template

---
+++ MVC Project Template/PBDesk.MVCProjectTemplate/PBDesk.MVCProjectTemplate/App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PBDesk.MVCProjectTemplate
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/toastr.js")
+                .Include("~/Scripts/toastr-custom.js")
+                .Include("~/Scripts/PBDeskUtils.js")
+                .Include("~/Scripts/Custom.js")                
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/angular")
+                .Include("~/scripts/angular.js")
+                .Include("~/scripts/angular-*")
+                
+                );
+           
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.cerulean.css",                      
+                      "~/Content/font-awesome.css",
+                      "~/Content/toastr.css",
+                      "~/Content/site.css"
+                      ));
+
+
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+
+
+        }
+    }
+}
